8a91469cf89a917bec32d5d1679a08901cd32b68
Cameron Reikes <cameronreikes@gmail.com>
2022-11-07 22:10:41 -0800

    Increase gamestate limit

--- types.h
+++ types.h
@@ -16,7 +16,7 @@
 #define THRUSTER_FORCE 12.0f
 #define THRUSTER_ENERGY_USED_PER_SECOND 0.005f
 #define VISION_RADIUS 16.0f
-#define MAX_BYTES_SIZE 1024 * 128 // maximum size of serialized gamestate buffer
+#define MAX_BYTES_SIZE 1024 * 512 // maximum size of serialized gamestate buffer
 #define SUN_RADIUS 10.0f
 #define INSTANT_DEATH_DISTANCE_FROM_SUN 2000.0f
 #define SUN_POS ((V2){50.0f,0.0f})
