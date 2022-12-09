cc184ad7f8e64fefc5801f482bbc3fc1fdb67e94
Cameron Reikes <cameronreikes@gmail.com>
2022-12-08 22:46:21 -0800

    Add license

--- release_all.bat
+++ release_all.bat
@@ -1,7 +1,7 @@
 git push
 call build_release.bat
 call update_server.bat
-tar.exe -a -c -f releases\flight-nonumber.zip flight_release.exe loaded
+tar.exe -a -c -f releases\flight-nonumber.zip flight_release.exe loaded LICENSE.txt
 echo "Now test flight-nonumber and make sure it works. Once everything is confirmed to be working:"
 echo "1. Increment the GIT_RELEASE_TAG in buildsettings.h"
 echo "2. Add everything to git and commit"
